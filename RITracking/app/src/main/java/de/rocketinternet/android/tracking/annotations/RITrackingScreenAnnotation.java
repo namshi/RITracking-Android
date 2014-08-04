495a36802d39f11a85b0a208ccd8880cb034694a
Alessandro Balocco <alessandro.balocco@rocket-internet.de>
2014-08-04 20:01:33 +0200

    Add documentation to screen annotation class

--- RITracking/app/src/main/java/de/rocketinternet/android/tracking/annotations/RITrackingScreenAnnotation.java
+++ RITracking/app/src/main/java/de/rocketinternet/android/tracking/annotations/RITrackingScreenAnnotation.java
@@ -7,6 +7,10 @@ import java.lang.annotation.Target;
 
 /**
  * @author alessandro.balocco
+ *
+ * This annotation allows users of the library to define a screen name. By doing that in Activities or
+ * Fragments that extend the base ones, the screen name will be automatically sent to the registered
+ * trackers.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
